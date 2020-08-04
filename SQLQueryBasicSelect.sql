932e580332251dc725a6a48d8fae413c6f565851
chriscutting <34825551+chriscutting@users.noreply.github.com>
2020-08-04 10:25:45 -0400

    Some Basic SQL
    I normally don't add my queries in here but I may as well show that I can do some basic SQL. Learning more every day.

---
+++ SQLQueryBasicSelect.sql
@@ -0,0 +1,4 @@
+SELECT EmployeeID, LastName, FirstName, Title, Region
+FROM Employees
+WHERE Region='WA'
+ORDER BY LastName ASC;
---
+++ SQLQueryImport.sql
@@ -0,0 +1,12 @@
+USE Movies_2
+
+Create Table Movies_Import_Temp
+(Title varchar(max),
+Director_FirstName varchar(max),
+Director_LastName varchar(max),
+Genre varchar(max),
+Star_FirstName varchar(max),
+Star_LastName varchar(max),
+Rating decimal(4,1),
+Producer_FirstName varchar(max),
+Producer_LastName varchar(max));
